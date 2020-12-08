0c68969bb717c5238d4b2eeb085354e799bdef34
tremelow <tremelow+gitk@protonmail.com>
2020-12-08 15:46:00 +0100

    Clean up
    Added day 1 in Julia and renamed python files. Also *hopefully* ignored inputs

---
+++ day1/day1_julia.jl
@@ -0,0 +1,23 @@
+function extract_data(filename)
+    open(filename) do f
+        return parse.(Int, readlines(f))
+    end
+end
+
+function ex1(data, tgt)
+    for (n1, n2) in Iterators.product(data, data)
+        n1 + n2 == tgt && return n1*n2
+    end
+    return -1
+end
+
+function ex2(data, tgt)
+    for (n1,n2,n3) in Iterators.product(data, data, data)
+        n1 + n2 + n3 == tgt && return n1*n2*n3
+    end
+    return -1
+end
+
+data = extract_data("day1_input")
+ex1(data, 2020)
+ex2(data, 2020)
---
+++ day3/day3_python.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 13:41:41 2020
+
+@author: tremelow
+"""
+from functools import reduce
+
+
+def __main__():
+    file = open("day3_input","r")
+    
+    data = [line for line in file.read().split('\n')[:-1]]
+    tree = "#"
+    
+    direction = (1,3)
+    
+    i,j = 0, 0
+    nb_trees = 0
+    while True:
+        i += direction[0]
+        if not i < len(data):
+            break
+        
+        j = (j+direction[1])%len(data[i])
+        nb_trees += (data[i][j] == tree)
+    print(nb_trees)
+    
+    directions = [(1,1), (1,3), (1,5), (1,7), (2,1)]
+    nb_trees = [0 for _ in directions]
+    for (n,d) in enumerate(directions):
+        i,j = 0, 0
+        while True:
+            i += d[0]
+            if not i < len(data):
+                break
+            j = (j+d[1])%len(data[i])
+            nb_trees[n] += (data[i][j] == tree)
+            
+    print(nb_trees)
+    print(reduce(lambda x,y: x*y, nb_trees, 1))
+
+        
+        
+        
+
+__main__()
