86211aaf7b29de911b608ce23db17fe89d9c99f8
tremelow <tremelow+gitk@protonmail.com>
2020-12-09 21:38:02 +0100

    Updated day 7 (Julia) to make graph structure obvious

--- day7_julia.jl
+++ day7_julia.jl
@@ -1,53 +1,49 @@
 mutable struct Node
-    id :: String
-    l  :: Dict{Node,Int}
-    p  :: Dict{Node,Int} # parents
-    Node() = new("", Dict{String, Int}(), Dict{String, Int}())
-    function Node(s::AbstractString)
-        new(s, Dict{String, Int}(), Dict{String, Int}())
-    end
+    prv :: Dict{Node,Int} # origins with weights
+    nxt :: Dict{Node,Int} # destinations with weigths
+    Node() = new(Dict{String, Int}(), Dict{String, Int}())
 end
 
 function read_entry(line)
     root, other = split(line, " bags contain ")
-    # build dictionary of ids and number for each leaf
-    re_leaves = eachmatch(r"(?<nb>\d+)\s(?<id>.*?)\sbag.?\b", other)
-    r2l = l -> l[:id] => parse(Int, l[:nb])
-    leaves = Dict{String,Int}([r2l(l) for l in re_leaves]... )
-    return root, leaves
+    # build dictionary of ids and weight for each destination
+    re_dests = eachmatch(r"(?<nb>\d+)\s(?<id>.*?)\sbag.?\b", other)
+    re2node = n -> n[:id] => parse(Int, n[:nb])
+    dests = Dict{String,Int}( [re2node(n) for n in re_dests]... )
+    return root, dests
 end
 
-function extract_nodes(filename)
+function extract_graph(filename)
     nodes = Dict{String,Node}()
     open(filename) do f
         for line in eachline(f)
-            # get direct relationship between root and leaves
-            root, leaves = read_entry(line)
+            # get direct relationship between root and dest
+            root, dests = read_entry(line)
 
             # if root does not exist, create root
-            parent = get!(nodes, root, Node(root))
+            orig = get!(nodes, root, Node())
             
             ## invert the relationship, link 
-            for (id, nb) in leaves
-                leaf = get!(nodes, id, Node(id))
-                push!(parent.l, leaf => nb)
-                push!(leaf.p, parent => nb)
+            for (id, nb) in dests
+                dest = get!(nodes, id, Node())
+                push!(orig.nxt, dest => nb)
+                push!(dest.prv, orig => nb)
             end
         end
     end
     return nodes
 end
 
-nodes = extract_nodes("day7_input")
+nodes = extract_graph("day7_input")
 
-function all_parents(node)
-    return union([p.first.id for p in node.p], 
-                    [all_parents(p.first) for p in node.p]...)
+function all_origins(node)
+    return union([p for (p,_) in node.prv], 
+                    [all_origins(p) for (p,_) in node.prv]...)
 end
-println(length(all_parents(nodes["shiny gold"])))
+println(length(all_origins(nodes["shiny gold"])))
 
-function sum_leaves(node)
-    return 1 + reduce(+, [w*sum_leaves(l) for (l,w) in node.l], init=0)
+function sum_dests(node)
+    return 1 + reduce(+, [w*sum_dests(n) for (n,w) in node.nxt], init=0)
 end
 # -1 to not count the shiny gold bag
-println(sum_leaves(nodes["shiny gold"])-1)
+println(sum_dests(nodes["shiny gold"])-1)
